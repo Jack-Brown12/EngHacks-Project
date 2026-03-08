b8e28d4de343619dcfcd4191d11d74f0a9b690b8
Jack-Brown12 <j84brown@uwaterloo.ca>
2026-03-07 19:00:59 -0500

    Changes RIS score weighting to prioritize common knowledge and ignore optional skills

--- RIS_Calculator.py
+++ RIS_Calculator.py
@@ -5,30 +5,30 @@ def RIS_calculator(user_resume_skills, market_analysis):
     user_skills_set = {s.lower() for s in user_resume_skills}
 
     for skill in market_analysis["core_skills"]:
-        max_possible_score += 1
+        max_possible_score += 10
         if skill.lower() in user_skills_set:
-            score += 1
+            score += 10
         else:
-            score -= 3
+            score -= 20
 
     for skill in market_analysis["optional_skills"]:
-        max_possible_score += 2
+        max_possible_score += 5
         if skill.lower() in user_skills_set:
-            score += 2
+            score += 5
         else: 
             score -= 2
 
     
     for skill in market_analysis["rare_skills"]:
-        max_possible_score += 3
+        max_possible_score += 0
         if skill.lower() in user_skills_set:
-            score += 3  
+            score += 25
         else: 
-            score -= 1
+            score -= 0
 
-           
+    if score <= 0: return 0
+       
 
-    final_percentage = ((score / max_possible_score) + 1) / 2 * 100
+    final_percentage = (score / max_possible_score) * 100
     
-    
-    return round(final_percentage, 2)
+    return round(max(0, final_percentage), 2)
